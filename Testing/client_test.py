ff4fbc4d1905e3d1ea108626e4dc7ce46ae628ad
nathaniel <nagoldfarb@wpi.edu>
2019-08-22 20:22:10 -0400

    working with new AMBF scripts

---
+++ Testing/client_test.py
@@ -0,0 +1,15 @@
+# Import the Client from ambf_client package
+from ambf_client import Client
+import time
+
+# Create a instance of the client
+_client = Client()
+
+# Connect the client which in turn creates callable objects from ROS topics
+# and initiates a shared pool of threads for bi-directional communication
+_client.connect()
+
+print('\n\n----')
+raw_input("We can see what objects the client has found. Press Enter to continue...")
+# You can print the names of objects found. We should see all the links found
+print(_client.get_obj_names())
