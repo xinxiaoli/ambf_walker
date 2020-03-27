9087e91cb8726522805c24204746bb4d193594a9
Nathaniel Goldfarb <nathanielgoldfarb@pop-os.localdomain>
2020-03-27 16:04:55 -0400

    updated AMBF model

--- ambf_models/run.sh
+++ ambf_models/run.sh
@@ -1,2 +1,2 @@
 cd ~/ambf/bin/lin-x86_64
-./ambf_simulator -a /home/nathanielgoldfarb/ambf_walker/ambf_models/exoskeleton.yaml
+./ambf_simulator -a /home/nathanielgoldfarb/ambf_walker/ambf_models/LARRE.yaml
