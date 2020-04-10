71f3d9b06255d528a772400a6c5eb4e8c6cee663
Nicholas Pacheco <nepacheco@wpi.edu>
2020-04-10 13:05:15 -0400

    Started creating rbdl model of Nathaniels example

--- Model/Human.py
+++ Model/Human.py
@@ -32,6 +32,87 @@ class Human(Model.Model):
     def dynamic_model(self, total_mass, height):
 
         model = rbdl.Model()
+        bodies = {}
+        mass = {}
+        com = {}
+        inertia = {}
+        bodies["right"] = {}
+        bodies["left"] = {}
+        segments = ["thigh", "shank", "foot"]
+
+        mass["body"] = 2.37
+        mass["head"] = 1.00
+        mass["right_arm_top"] = 1.00
+        mass["left_arm_top"] = 1.00
+        mass["right_arm_bot"] = 1.00
+        mass["left_arm_bot"] = 1.00
+        mass["right_thigh"] = 2.11
+        mass["left_thigh"] = 2.11
+        mass["right_shin"] = 1.28
+        mass["left_shin"] = 1.28
+        mass["right_foot"] = 0.86
+        mass["left_foot"] = 0.86
+        parent_dist = {}
+        parent_dist["body"] = np.array([0.0, 0.0, 0.0])
+
+        parent_dist["left_thigh"] = np.array([0.237, -0.124, -0.144])
+        parent_dist["left_shank"] = np.array([0.033, -0.03, -0.436])
+        parent_dist["left_foot"] = np.array([0.02, -0.027, -0.39])
+
+        parent_dist["right_thigh"] = np.array([-0.237, -0.124, -0.144])
+        parent_dist["right_shank"] = np.array([0.033, -0.03, -0.436])
+        parent_dist["right_foot"] = np.array([0.02, -0.027, -0.39])
+
+        inertia["hip"] = np.diag([0.0, 0.0, 0.0])
+
+        inertia["left_thigh"] = np.diag([0.0, 0.0, 0.07])
+        inertia["left_shank"] = np.diag([0.18, 0.18, 0.0])
+        inertia["left_foot"] = np.diag([0.07, 0.07, 0.0])
+
+        inertia["right_thigh"] = np.diag([0.0, 0.00, 0.07])
+        inertia["right_shank"] = np.diag([0.18, 0.18, 0.0])
+        inertia["right_foot"] = np.diag([0.07, 0.07, 0.0])
+
+        com["hip"] = np.array([0.00, -0.02, 0.18])
+        com["left_thigh"] = np.array([0.02, 0.01, -0.09])
+        com["left_shank"] = np.array([-0.02, -0.007, 0.06])
+        com["left_foot"] = np.array([0.08, -0.06, 0.04])
+
+        com["right_thigh"] = np.array([-0.02, 0.01, -0.09])
+        com["right_shank"] = np.array([0.02, -0.007, 0.06])
+        com["right_foot"] = np.array([0.08, -0.06, 0.04])
+
+        hip_body = rbdl.Body.fromMassComInertia(mass["hip"], com["hip"], inertia["hip"])
+        for segs in segments:
+            bodies["right_" + segs] = rbdl.Body.fromMassComInertia(mass["right_" + segs], com["right_" + segs],
+                                                                   inertia["right_" + segs])
+            bodies["left_" + segs] = rbdl.Body.fromMassComInertia(mass["left_" + segs], com["left_" + segs],
+                                                                  inertia["left_" + segs])
+
+        xtrans = rbdl.SpatialTransform()
+        xtrans.r = np.array([0.0, 0.0, 0.0])
+        xtrans.E = np.eye(3)
+
+        self.hip = model.AddBody(0, xtrans, rbdl.Joint.fromJointType("JointTypeFixed"), hip_body, "hip")
+        joint_rot_z = rbdl.Joint.fromJointType("JointTypeRevoluteX")
+
+        xtrans.r = parent_dist["left_thigh"]
+        self.left_thigh = model.AddBody(self.hip, xtrans, joint_rot_z, bodies["left_thigh"], "left_thigh")
+        xtrans.E = np.eye(3)
+        xtrans.r = parent_dist["left_shank"]
+        self.left_shank = model.AddBody(self.left_thigh, xtrans, joint_rot_z, bodies["left_shank"], "left_shank")
+        xtrans.r = parent_dist["left_foot"]
+        self.left_foot = model.AddBody(self.left_shank, xtrans, joint_rot_z, bodies["left_foot"], "left_foot")
+
+        xtrans.r = parent_dist["right_thigh"]
+        self.right_thigh = model.AddBody(self.hip, xtrans, joint_rot_z, bodies["right_thigh"], "right_thigh")
+        xtrans.E = np.eye(3)
+        xtrans.r = parent_dist["right_shank"]
+        self.right_shank = model.AddBody(self.right_thigh, xtrans, joint_rot_z, bodies["right_shank"], "right_shank")
+        xtrans.r = parent_dist["right_foot"]
+        self.right_foot = model.AddBody(self.right_shank, xtrans, joint_rot_z, bodies["right_foot"], "right_foot")
+
+        model.gravity = np.array([0, 0, -9.81])
 
         return model
 
