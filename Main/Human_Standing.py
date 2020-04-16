0728347f748dc1b6485e663852c5555c8dabfb8e
jdalmeida <jfdalmeida@wpi.edu>
2020-04-16 12:38:02 -0400

    renamed calf and added beginnings of standing script

--- Main/Human_Standing.py
+++ Main/Human_Standing.py
@@ -1,6 +1,6 @@
 import sys
 from ambf_client import Client
-from Controller import PDController
+from Controller.PDController import PDController
 import numpy as np
 import time
 from Model.Human import Human
@@ -13,10 +13,18 @@ _client.connect()
 print(_client.get_obj_names())
 
 # body_handle = _client.get_obj_handle('body')
-h = Human(_client, 50, 1.5)
+# h = Human(_client, 50, 1.5)
 q_goal = [0.0] * 7
 qd_goal = [0.0] * 7
 qdd_goal = [0.0] * 7
+cmd = np.asarray([0.0] * 6)
+
+gains = ( [ 510,36.8],[565,42.89],[354,45.2],[ 510,36.8],[565,42.89],[354,45.2])
+
+Kp = np.array([0,gains[0][0],gains[1][0],gains[2][0],gains[3][0],gains[4][0],gains[5][0] ])
+Kd = np.array([0,gains[0][1],gains[1][1],gains[2][1],gains[3][1],gains[4][1],gains[5][1] ])
+
+Controller = PDController(Kp, Kd)
 
 
 def go(count):
@@ -30,20 +38,20 @@ def go(count):
         qdd_goal[i] = 0
 
     aq = qdd_goal + Controller.calc(q_goal - q, qd_goal - qd)
-    tau = h.calculate_dynamics(q_d  , qd_d, aq)
+    tau = h.calculate_dynamics(aq)
 
     for i in range(6):
         cmd[i] = tau[i + 1]
 
-    h.send_command(cmd)
+    return cmd
 
-    time.sleep(0.01)
 
-def send_command(self, tau):
-    cmd = ambf.ObjectCmd()
-    cmd.publish_joint_names = True
-    cmd.publish_joint_positions = True
-    cmd.enable_position_controller = False
-    cmd.joint_cmds = tau
-    self.tau_pub.publish(cmd)
+
+
+
+
+
+
+
+
 
--- Model/Human.py
+++ Model/Human.py
@@ -18,18 +18,11 @@ class Human(Model.Model):
         # inits dynamic model and joints for leg
         super(Human, self).__init__(client, mass, height)
 
-        self._handle = self._client.get_obj_handle('body')
+        self.handle = self._client.get_obj_handle('body')
+        self.body_children = ["left_thigh", "left_foot", "left_calf", "left_arm_bot", "left_arm_top", "left_hand",
+                         "right_thigh", "right_foot", "right_calf", "right_arm_bot", "right_arm_top", "right_hand",
+                         "head"]
 
-        """
-        state of each segment, format in order of creation:
-        right thigh
-        left thigh
-        right shin
-        left shin
-        right foot
-        left foot
-        body
-        """
         # num_of_segments should be initialized with the dynamical model, which is created in the constructor
         self.q = self.num_of_segments * [0.0]
         self.qd = self.num_of_segments * [0.0]
@@ -46,25 +39,40 @@ class Human(Model.Model):
     def state(self, value):
         self._state = np.concatenate(value)
 
+    @q.setter
+    def q(self, value):
+        # TODO: transform RBDL to AMBF
+        self._q = np.asarray(value)
+
+    @qd.setter
+    def qd(self, value):
+        # TODO: transform RBDL to AMBF
+        value[2] *= -1
+        value[5] *= -1
+        self._qd = np.asarray(value)
+
+
     def dynamic_model(self, total_mass, height):
         # TODO: Add joints and bodies for arms and head
 
         model = rbdl.Model()
         bodies = {}
+        parent_dist = {}
         mass = {}
         com = {}
         inertia = {}
         bodies["right"] = {}
         bodies["left"] = {}
-        segments = ["thigh", "shank", "foot"]
+        segments = ["thigh", "calf", "foot"]
 
         # percent total body weight from average in de Leva
         per_head = 6.81
         per_trunk = 43.02
         per_upper_arm = 2.63
-        per_lower_arm = 1.5 + 0.585     # forearm + hand
+        per_lower_arm = 1.5
+        per_hand = 0.585
         per_thigh = 14.47
-        per_shank = 4.57
+        per_calf = 4.57
         per_foot = 1.33
 
         mass["head"] = total_mass * per_head
@@ -73,46 +81,48 @@ class Human(Model.Model):
         mass["left_arm_top"] = total_mass * per_upper_arm
         mass["right_arm_bot"] = total_mass * per_lower_arm
         mass["left_arm_bot"] = total_mass * per_lower_arm
+        mass["right_hand"] = total_mass * per_hand
+        mass["left_hand"] = total_mass * per_hand
         mass["right_thigh"] = total_mass * per_thigh
         mass["left_thigh"] = total_mass * per_thigh
-        mass["right_shank"] = total_mass * per_shank
-        mass["left_shank"] = total_mass * per_shank
+        mass["right_calf"] = total_mass * per_calf
+        mass["left_calf"] = total_mass * per_calf
         mass["right_foot"] = total_mass * per_foot
         mass["left_foot"] = total_mass * per_foot
 
-        parent_dist = {}
         parent_dist["body"] = np.array([0.0, 0.0, 0.0])
 
         parent_dist["left_thigh"] = np.array([0.237, -0.124, -0.144])
-        parent_dist["left_shank"] = np.array([0.033, -0.03, -0.436])
+        parent_dist["left_calf"] = np.array([0.033, -0.03, -0.436])
         parent_dist["left_foot"] = np.array([0.02, -0.027, -0.39])
 
         parent_dist["right_thigh"] = np.array([-0.237, -0.124, -0.144])
-        parent_dist["right_shank"] = np.array([0.033, -0.03, -0.436])
+        parent_dist["right_calf"] = np.array([0.033, -0.03, -0.436])
         parent_dist["right_foot"] = np.array([0.02, -0.027, -0.39])
 
         self.num_of_segments = len(parent_dist)
 
-        inertia["hip"] = np.diag([0.0, 0.0, 0.0])
+        inertia["body"] = np.diag([0.0, 0.0, 0.0])
 
         inertia["left_thigh"] = np.diag([0.0, 0.0, 0.07])
-        inertia["left_shank"] = np.diag([0.18, 0.18, 0.0])
+        inertia["left_calf"] = np.diag([0.18, 0.18, 0.0])
         inertia["left_foot"] = np.diag([0.07, 0.07, 0.0])
 
         inertia["right_thigh"] = np.diag([0.0, 0.00, 0.07])
-        inertia["right_shank"] = np.diag([0.18, 0.18, 0.0])
+        inertia["right_calf"] = np.diag([0.18, 0.18, 0.0])
         inertia["right_foot"] = np.diag([0.07, 0.07, 0.0])
 
-        com["hip"] = np.array([0.00, -0.02, 0.18])
+        com["body"] = np.array([0.00, -0.02, 0.18])
+
         com["left_thigh"] = np.array([0.02, 0.01, -0.09])
-        com["left_shank"] = np.array([-0.02, -0.007, 0.06])
+        com["left_calf"] = np.array([-0.02, -0.007, 0.06])
         com["left_foot"] = np.array([0.08, -0.06, 0.04])
 
         com["right_thigh"] = np.array([-0.02, 0.01, -0.09])
-        com["right_shank"] = np.array([0.02, -0.007, 0.06])
+        com["right_calf"] = np.array([0.02, -0.007, 0.06])
         com["right_foot"] = np.array([0.08, -0.06, 0.04])
 
-        hip_body = rbdl.Body.fromMassComInertia(mass["body"], com["hip"], inertia["hip"])
+        body_rbdl = rbdl.Body.fromMassComInertia(mass["body"], com["body"], inertia["body"])
         for segs in segments:
             bodies["right_" + segs] = rbdl.Body.fromMassComInertia(mass["right_" + segs], com["right_" + segs],
                                                                    inertia["right_" + segs])
@@ -123,28 +133,28 @@ class Human(Model.Model):
         xtrans.r = np.array([0.0, 0.0, 0.0])
         xtrans.E = np.eye(3)
 
-        self.hip = model.AddBody(0, xtrans, rbdl.Joint.fromJointType("JointTypeFixed"), hip_body, "hip")
+        self.body = model.AddBody(0, xtrans, rbdl.Joint.fromJointType("JointTypeFixed"), body_rbdl, "hip")
         joint_rot_z = rbdl.Joint.fromJointType("JointTypeRevoluteX")
 
         xtrans.r = parent_dist["left_thigh"]
-        self.left_thigh = model.AddBody(self.hip, xtrans, joint_rot_z, bodies["left_thigh"], "left_thigh")
+        self.left_thigh = model.AddBody(self.body, xtrans, joint_rot_z, bodies["left_thigh"], "left_thigh")
 
         xtrans.E = np.eye(3)
-        xtrans.r = parent_dist["left_shank"]
-        self.left_shank = model.AddBody(self.left_thigh, xtrans, joint_rot_z, bodies["left_shank"], "left_shank")
+        xtrans.r = parent_dist["left_calf"]
+        self.left_calf = model.AddBody(self.left_thigh, xtrans, joint_rot_z, bodies["left_calf"], "left_calf")
 
         xtrans.r = parent_dist["left_foot"]
-        self.left_foot = model.AddBody(self.left_shank, xtrans, joint_rot_z, bodies["left_foot"], "left_foot")
+        self.left_foot = model.AddBody(self.left_calf, xtrans, joint_rot_z, bodies["left_foot"], "left_foot")
 
         xtrans.r = parent_dist["right_thigh"]
-        self.right_thigh = model.AddBody(self.hip, xtrans, joint_rot_z, bodies["right_thigh"], "right_thigh")
+        self.right_thigh = model.AddBody(self.body, xtrans, joint_rot_z, bodies["right_thigh"], "right_thigh")
 
         xtrans.E = np.eye(3)
-        xtrans.r = parent_dist["right_shank"]
-        self.right_shank = model.AddBody(self.right_thigh, xtrans, joint_rot_z, bodies["right_shank"], "right_shank")
+        xtrans.r = parent_dist["right_calf"]
+        self.right_calf = model.AddBody(self.right_thigh, xtrans, joint_rot_z, bodies["right_calf"], "right_calf")
 
         xtrans.r = parent_dist["right_foot"]
-        self.right_foot = model.AddBody(self.right_shank, xtrans, joint_rot_z, bodies["right_foot"], "right_foot")
+        self.right_foot = model.AddBody(self.right_calf, xtrans, joint_rot_z, bodies["right_foot"], "right_foot")
 
         model.gravity = np.array([0, 0, -9.81])
 
@@ -157,14 +167,5 @@ class Human(Model.Model):
     def calculate_dynamics(self, qdd):
         # Overrides as human has more states. Not sure if really need since upperbody will not be controlled?
         tau = np.asarray([0.0] * self._joint_num)
-        rbdl.InverseDynamics(self._model, self.q[0:6], self.qd[0:6], qdd[0:6], tau)
+        rbdl.InverseDynamics(self._model, self.q, self.qd, qdd, tau)
         return tau
-
-    def update_state(self, q, qd):
-        self.get_left_leg().hip.angle.z = q[0]
-        self.get_left_leg().knee.angle.z = q[1]
-        self.get_left_leg().ankle.angle.z = q[2]
-
-        self.get_right_leg().hip.angle.z = q[3]
-        self.get_right_leg().knee.angle.z = q[4]
-        self.get_right_leg().ankle.angle.z = q[5]
