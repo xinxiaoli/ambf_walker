e7849252063cbcf353a684241fc7258a0e8147d2
jdalmeida <jfdalmeida@wpi.edu>
2020-04-29 22:56:10 -0400

    slowly lower and fixed juggler order

--- Main/Human_Standing.py
+++ Main/Human_Standing.py
@@ -79,51 +79,52 @@ def loop(tf=5):
 
     hip_traj.create_traj(0, hip_goal, 0, 0, tf)
     knee_traj.create_traj(0, knee_goal, 0, 0, tf)
-    ankle_traj.create_traj(0, -0.3491, 0, 0, tf)
+    ankle_traj.create_traj(0, ankle_goal, 0, 0, tf)
 
     start = rospy.get_time()
     t = 0
-    msg = Float32MultiArray()
 
     # run the controller for the given time
     print("Starting loop")
     while abs(t) < tf:
-        t = rospy.get_time() - start
-
-        q_goal = np.array([0.0] * num_joints)
-        qd_goal = np.array([0.0] * num_joints)
-        qdd_goal = np.array([0.0] * num_joints)
-
-        # get traj value
-        for i in range(len(trajs)):
-            traj_q, traj_qd, traj_qdd = trajs[i].get_traj(t)   # values from traj for specific joint
-
-            # set the traj values to the correct joint state
-            q_goal[left_order[leg_segs[i]]] = traj_q
-            q_goal[right_order[leg_segs[i]]] = traj_q
-            qd_goal[left_order[leg_segs[i]]] = traj_qd
-            qd_goal[right_order[leg_segs[i]]] = traj_qd
-            qdd_goal[left_order[leg_segs[i]]] = traj_qdd
-            qdd_goal[right_order[leg_segs[i]]] = traj_qdd
-
-        # Get current states
-        msg.data = q_goal
-        pub_goal.publish(msg)
-
-        q = human.q
-        qd = human.qd
-        aq = qdd_goal + Controller.get_tau(q_goal - q, qd_goal - qd)
-        # print('q')
-        # print(q)
-        # Calc tau from dynamical model
-        tau = human.calculate_dynamics(aq)
-        # print('tau')
-        # print(tau)
-        human.update_torque(tau)
-        t = rospy.get_time() - start
-
+        t = control_loop(start, Controller)
     print("5 second loop over")
 
+
+def control_loop(start, Controller):
+    t = rospy.get_time() - start
+
+    q_goal = np.array([0.0] * num_joints)
+    qd_goal = np.array([0.0] * num_joints)
+    qdd_goal = np.array([0.0] * num_joints)
+    msg = Float32MultiArray()
+
+    # get traj value
+    for i in range(len(trajs)):
+        traj_q, traj_qd, traj_qdd = trajs[i].get_traj(t)  # values from traj for specific joint
+
+        # set the traj values to the correct joint state
+        q_goal[left_order[leg_segs[i]]] = traj_q
+        q_goal[right_order[leg_segs[i]]] = traj_q
+        qd_goal[left_order[leg_segs[i]]] = traj_qd
+        qd_goal[right_order[leg_segs[i]]] = traj_qd
+        qdd_goal[left_order[leg_segs[i]]] = traj_qdd
+        qdd_goal[right_order[leg_segs[i]]] = traj_qdd
+
+    # publish
+    msg.data = q_goal
+    pub_goal.publish(msg)
+
+    # Get current states
+    q = human.q
+    qd = human.qd
+    aq = qdd_goal + Controller.get_tau(q_goal - q, qd_goal - qd)
+
+    # Calc tau from dynamical model
+    tau = human.calculate_dynamics(aq)
+    human.update_torque(tau)
+    t = rospy.get_time() - start
+    return t
 
 def set_body(h=.2,r=1.3):
     """
@@ -131,7 +132,6 @@ def set_body(h=.2,r=1.3):
     """
     body.set_pos(0, 0, h)
     body.set_rpy(r, 0, 0)
-
 
 def free_body():
     """
@@ -141,10 +141,41 @@ def free_body():
     body._cmd.enable_position_controller = False
     remove_torques()
 
-
 def remove_torques():
     for i in range(len(body.get_joint_names())):
         body.set_joint_effort(i,0)
+
+def slowly_lower():
+    """
+    start body above ground then slowly lower until feet are touching (0,0,0)
+    release position controller and continue control loop
+    """
+    h = .2
+    set_body(h)
+    start = rospy.get_time()
+    t = 0
+    tf = 10
+    dh = .0001
+
+    Controller = init_k_vals()  # initialize the controller values
+
+    hip_traj.create_traj(0, hip_goal, 0, 0, tf)
+    knee_traj.create_traj(0, knee_goal, 0, 0, tf)
+    ankle_traj.create_traj(0, ankle_goal, 0, 0, tf)
+
+    # run the controller for the given time
+    print("Starting loop")
+    while abs(t) < tf:
+        if h >= 0:
+            h -= dh
+            set_body(h)
+        elif h <= 0 and h is not -1:
+            print('At ground, free body')
+            free_body()
+            h = -1
+
+        t = control_loop(start, Controller)
+    print("loop done")
 
 
 set_body()
